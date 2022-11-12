805539ba51fbebffa35fac3739a0171cc4b5ae54
tbeatzz <tbeatzz20k@gmail.com>
2022-11-12 14:18:55 -0300

    juego

---
+++ js/app.js
@@ -0,0 +1,7 @@
+window.onload = function () {
+  let overlayUser = document.querySelector(".modal__overlay");
+  let selectUser = document.querySelector(".modal__user");
+
+  overlayUser.classList.add("modal__overlay--visible");
+  selectUser.classList.add("modal__user--visible");
+};
